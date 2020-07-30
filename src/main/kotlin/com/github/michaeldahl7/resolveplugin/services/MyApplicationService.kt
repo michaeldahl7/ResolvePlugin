62d53abade09ee8f439847bc0f6390be95e9ee1f
GitHub Action <action@github.com>
2020-07-30 20:18:16 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/michaeldahl7/resolveplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.michaeldahl7.resolveplugin.services
+
+import com.github.michaeldahl7.resolveplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
